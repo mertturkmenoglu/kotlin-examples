20ba23af7f361f19a4c36b534b8a979cde521546
Mert <mertturkmenoglu99@gmail.com>
2019-04-02 06:03:45 +0300

    Add Animal Project Inheritance and interface test class Animal added

---
+++ Animal/Animal.kt
@@ -0,0 +1,38 @@
+package Animal
+
+abstract class Animal {
+
+    abstract val speciesName: String
+    abstract val age: Int
+    abstract val color: String
+
+
+}
+
+class Cat: Animal(), AnimalAction {
+
+    override val speciesName = "Kedi"
+    override val age = 0
+    override val color = "gray"
+    override fun eat() = println("Cats eat meat and drink milk.")
+    override fun sound() = println("Meow")
+
+}
+
+class Dog: Animal(), AnimalAction {
+
+    override val speciesName = "Köpek"
+    override val age = 0
+    override val color = "gray"
+    override fun eat() = println("Dogs eat meat and drink water.")
+    override fun sound() = println("Bark")
+
+}
+
+interface AnimalAction {
+    fun eat()
+    fun sound()
+}
+
+
+
