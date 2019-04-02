f9a15df8d5fa7ce0f336dffea0f0529b96a69514
Mert <mertturkmenoglu99@gmail.com>
2019-04-02 06:05:04 +0300

    Add Aquarium Project Udacity Kotlin course Aquarium example added

---
+++ Aquarium/main.kt
@@ -0,0 +1,35 @@
+package Aquarium
+
+fun main(args: Array<String>) {
+
+    buildAquarium()
+    makeFish()
+
+}
+
+fun buildAquarium() {
+
+    val myAquarium = Aquarium()
+
+    println("Length: ${myAquarium.length}\n" +
+            "Width: ${myAquarium.width}\n" +
+            "Height: ${myAquarium.height}")
+
+    myAquarium.height = 80
+
+    println("Height: ${myAquarium.height}")
+    println("Volume: ${myAquarium.volume}")
+
+}
+
+fun feedFish(fish: FishAction) {
+    fish.eat()
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${shark.color}\nPlecostomus: ${pleco.color}")
+}
+
