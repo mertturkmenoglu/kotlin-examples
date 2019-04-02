68f3db726ac6f51f59d8b1f05a601ede616079a9
Mert <mertturkmenoglu99@gmail.com>
2019-04-02 11:09:56 +0300

    Add language basics example Console argument and IO examples added

---
+++ kotlinBasics/src/Person.java
@@ -0,0 +1,9 @@
+import static java.lang.System.console;
+
+public class Person{
+
+    public static void main(String[] args) {
+        String a = console().readLine();
+        System.out.println(a);
+    }
+}
---
+++ kotlinInputTest/Main.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+fun main(args: Array<String>) {
+
+    print("Please enter an integer: ")
+    var number = (readLine() !!).toInt()
+
+    println("You entered: $number")
+    number ++
+    println("New value: $number")
+
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    var integer: Int = reader.nextInt()
+
+    println("You entered: $integer. I read it with Java...")
+
+
+}
