d9977d051a0dcb30bd034805622f8aca38217aaa
Mert <mertturkmenoglu99@gmail.com>
2019-04-02 06:06:22 +0300

    Add Array Test Codes Various array creation and operation codes added

---
+++ Array/arrayInputOutput.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    println("Please enter array size: ")
+    val n = readLine()!!.toInt()
+    var array1 = arrayOfNulls<Int>(n)
+
+    for(i in 0..n-1) {
+        print("Array[$i]: ")
+        array1[i] = readLine()!!.toInt()
+    }
+    println(array1.asList())
+
+//    val array2 = arrayOf(1,2,"wrong type")
+//    println(array2.asList())
+
+}
