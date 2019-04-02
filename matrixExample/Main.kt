4eac0e9a6f4159c5f1d2e655015e850b24414ce5
Mert <mertturkmenoglu99@gmail.com>
2019-04-02 11:25:03 +0300

    Add Matrix Example 2-dim array example added

---
+++ matrixExample/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val n = readLine()!!.toInt()
+    val mtr = Array(n) {Array(n) {0}}
+
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            mtr[i][j] = readLine()!!.toInt()
+        }
+    }
+
+    for (i in 0 until n) {
+        for (j in 0 until n) {
+            print("${mtr[i][j]}\t")
+        }
+        println()
+    }
+
+}
