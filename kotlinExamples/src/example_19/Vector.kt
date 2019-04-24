4179f3b61030b2493f816a1eaeace4957b6ae3b5
Mert <mertturkmenoglu99@gmail.com>
2019-04-24 22:16:35 +0300

    Update Operator Overloading Example

--- kotlinExamples/src/example_19/Vector.kt
+++ kotlinExamples/src/example_19/Vector.kt
@@ -1,2 +1,12 @@
 package example_19
 
+class Vector(var x: Float = 0f, var y: Float = 0f) {
+    operator fun plus(other: Vector) = Vector(x + other.x, y + other.y)
+    operator fun minus(other: Vector) = Vector(x - other.x, y - other.y)
+    operator fun times(other: Vector) = x * other.x + y * other.y
+    operator fun div(num: Float) = Vector(x / num, y / num)
+
+    fun printVector() {
+        println("$x\t$y")
+    }
+}
