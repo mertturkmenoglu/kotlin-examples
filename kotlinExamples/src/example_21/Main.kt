743bd93517d0a404fd73d909c493e31c9a3fe3d3
Mert <mertturkmenoglu99@gmail.com>
2019-04-25 21:03:24 +0300

    Add companion object example

---
+++ kotlinExamples/src/example_21/Main.kt
@@ -0,0 +1,8 @@
+package example_21
+
+// Companion object example
+// They are kinda similar to Java static methods
+fun main() {
+    MathClass.printPI()
+    MathClass.printE()
+}
---
+++ kotlinExamples/src/example_21/MathClass.kt
@@ -0,0 +1,8 @@
+package example_21
+
+class MathClass {
+    companion object {
+        fun printPI() = println("PI: ${Math.PI}")
+        fun printE() = println("E: ${Math.E}")
+    }
+}
