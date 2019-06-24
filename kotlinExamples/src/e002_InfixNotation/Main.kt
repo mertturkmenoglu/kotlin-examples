d28d2a9353038f8ea1885b12bdb8f6083d40d010
Mert <mertturkmenoglu99@gmail.com>
2019-06-24 14:23:04 +0300

    Add infix notation example

---
+++ kotlinExamples/src/e002_InfixNotation/Main.kt
@@ -0,0 +1,23 @@
+package e002_InfixNotation
+
+import kotlin.random.Random
+
+fun main() {
+    val age = Random.nextInt(10, 30)
+    val isSingle = Random.nextBoolean()
+
+    val result = (age < 18) or isSingle
+    println("Age: $age\tisSingle: $isSingle")
+    println(if (result) "Can't enter" else "Can enter")
+
+    val person1 = Bool(true)
+    val person2 = Bool(false)
+
+    print(person1 or person2)
+}
+
+class Bool(var value: Boolean = false) {
+    infix fun or(other: Bool): Boolean {
+        return value or other.value
+    }
+}
