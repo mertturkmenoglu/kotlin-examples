b284419cc0192b7915451a1469c3440814046812
Mert <mertturkmenoglu99@gmail.com>
2019-06-24 22:09:05 +0300

    Add lambda example

---
+++ kotlinExamples/src/e011_Lambda/Main.kt
@@ -0,0 +1,14 @@
+package e011_Lambda
+
+fun <T> foreach(array: Array<T>, func: (T) -> Unit) {
+    for (e in array) {
+        func(e)
+    }
+}
+
+fun main() {
+    val arr = arrayOf(1, 2, 3, 4, 5, 6)
+
+    foreach(arr) { num: Int -> println(num)}
+    foreach(arr) { num: Int -> println(num * num)}
+}
