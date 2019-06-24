75d86dbc17e864324f568c52d915216fd7eb5c9f
Mert <mertturkmenoglu99@gmail.com>
2019-06-24 16:26:43 +0300

    Add inheritance and constructors example

---
+++ kotlinExamples/src/e004_InheritanceAndConstructors/Main.kt
@@ -0,0 +1,32 @@
+package e004_InheritanceAndConstructors
+
+open class Person {
+    private var name: String = ""
+    private var age: Int = -1
+
+    constructor(name: String) {
+        println("Person  secondary constructor with 1 parameter called")
+        this.name = name
+    }
+
+    constructor(name: String, age: Int) : this(name){
+        println("Person  secondary constructor with 2 parameter called")
+        this.age = age
+    }
+}
+
+class Teacher : Person {
+    constructor(name: String) : super(name) {
+        println("Teacher secondary constructor with 1 parameter called")
+    }
+    constructor(name: String, age: Int) : super(name, age) {
+        println("Teacher secondary constructor with 2 parameter called")
+    }
+}
+
+fun main() {
+    val w1 = Teacher("Emily")
+    val w2 = Teacher("Emily", 35)
+    val p1 = Person("Emily")
+    val p2 = Person("Emily", 35)
+}
