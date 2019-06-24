f6e740468398566d3ec7d6120b93a84cc5a784f5
Mert <mertturkmenoglu99@gmail.com>
2019-06-24 16:35:50 +0300

    Add getter / setter example

--- kotlinExamples/src/e004_InheritanceAndConstructors/Main.kt
+++ kotlinExamples/src/e004_InheritanceAndConstructors/Main.kt
@@ -1,15 +1,24 @@
 package e004_InheritanceAndConstructors
 
 open class Person {
-    private var name: String = ""
-    private var age: Int = -1
+    private var name: String
+        get() {
+            return "Person: $field"
+        }
+
+    var age: Int = -1
+        set(value) {
+            if (value >= 20)
+                field = value
+        }
+
 
     constructor(name: String) {
         println("Person  secondary constructor with 1 parameter called")
         this.name = name
     }
 
-    constructor(name: String, age: Int) : this(name){
+    constructor(name: String, age: Int) : this(name) {
         println("Person  secondary constructor with 2 parameter called")
         this.age = age
     }
@@ -19,6 +28,7 @@ class Teacher : Person {
     constructor(name: String) : super(name) {
         println("Teacher secondary constructor with 1 parameter called")
     }
+
     constructor(name: String, age: Int) : super(name, age) {
         println("Teacher secondary constructor with 2 parameter called")
     }
@@ -29,4 +39,6 @@ fun main() {
     val w2 = Teacher("Emily", 35)
     val p1 = Person("Emily")
     val p2 = Person("Emily", 35)
+    p1.age = 32
+    print(p1.age)
 }
