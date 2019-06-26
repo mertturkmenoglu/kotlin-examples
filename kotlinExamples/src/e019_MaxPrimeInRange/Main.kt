c2becec9ee743db0ce38d2ab7c19a6b7932aa50e
Mert <mertturkmenoglu99@gmail.com>
2019-06-26 19:50:34 +0300

    Add prime number example

---
+++ kotlinExamples/src/e019_MaxPrimeInRange/Main.kt
@@ -0,0 +1,43 @@
+package e019_MaxPrimeInRange
+
+import kotlin.math.sqrt
+
+// Find the biggest prime number in given collection
+
+fun main() {
+    val arr = arrayOf(3, 5, 9, 12, 27, 43, 79, 86)
+    val max = maxPrime(arr.iterator())
+    print(max)
+}
+
+
+fun maxPrime(iterator: Iterator<Int>): Int {
+    var max = -1
+
+    for (i in iterator) {
+        if (i > max) {
+            if (isPrime(i)) {
+                max = i
+            }
+        }
+    }
+
+    return max
+}
+
+
+fun isPrime(n: Int): Boolean {
+    var i = 3
+    val sq = sqrt(n.toDouble())
+
+    if (n % 2 == 0)
+        return false
+
+    while (i <= sq) {
+        if (n % i == 0)
+            return false
+        i += 2
+    }
+
+    return true
+}
