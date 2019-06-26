7aac82b7ff3452a26a853d81116600f226a5eba8
Mert <mertturkmenoglu99@gmail.com>
2019-06-26 22:15:51 +0300

    Add binary and linear search implementation

---
+++ kotlinExamples/src/e022_Search/Main.kt
@@ -0,0 +1,33 @@
+package e022_Search
+
+fun <T: Comparable<T>> linearSearch(collection: Collection<T>, value: T): Int {
+    val iterator = collection.iterator().withIndex()
+    iterator.forEach {
+        if (it.value == value) {
+            return it.index
+        }
+    }
+    return -1
+}
+
+fun <T: Comparable<T>> binarySearch(collection: Collection<T>, value: T): Int {
+    var left = 0
+    var right = collection.size - 1
+
+    while(left <= right) {
+        val middle = (left + right) / 2
+        when {
+            value < collection.elementAt(middle) -> right = middle - 1
+            value > collection.elementAt(middle) -> left = middle + 1
+            else -> return middle
+        }
+    }
+
+    return -1
+}
+
+fun main() {
+    val arr = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    println(linearSearch(arr, 7))
+    println(binarySearch(arr, 3))
+}
