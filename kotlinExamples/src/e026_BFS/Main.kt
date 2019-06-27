2d63c709cc88f3d3a793371cee573b3037744c7c
Mert <mertturkmenoglu99@gmail.com>
2019-06-27 03:14:55 +0300

    Implement BFS algorithm

---
+++ kotlinExamples/src/e026_BFS/Main.kt
@@ -0,0 +1,23 @@
+package e026_BFS
+
+fun main() {
+    val graph = Graph<String>(4)
+
+    graph.addNode(Node(0, "A"))
+    graph.addNode(Node(1, "B"))
+    graph.addNode(Node(2, "C"))
+    graph.addNode(Node(3, "D"))
+
+    graph.addConnection(0, 1)
+    graph.addConnection(0, 2)
+    graph.addConnection(1, 2)
+    graph.addConnection(2, 3)
+
+    graph.printGraph()
+
+    val path = bfs(graph, 0, 3)
+
+    path?.forEach {
+        println(it.value)
+    }
+}
