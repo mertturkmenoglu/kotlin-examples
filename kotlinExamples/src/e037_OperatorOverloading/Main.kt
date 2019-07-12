3a6bf843c32d7c63f5082d805c972614662ec96b
Mert <mertturkmenoglu99@gmail.com>
2019-07-12 14:53:29 +0300

    Add operator overloading example

---
+++ kotlinExamples/src/e037_OperatorOverloading/Main.kt
@@ -0,0 +1,18 @@
+package e037_OperatorOverloading
+
+fun main() {
+    val c1 = Complex(3, 4)
+    println(c1)
+
+    val c2 = Complex(5, -12)
+    println(c2)
+
+    val c3 = c2 - c1
+    println(c3)
+
+    if (c2 < c1) {
+        println("$c2 is less than $c1")
+    } else {
+        println("$c2 is equal to or greater then $c1")
+    }
+}
