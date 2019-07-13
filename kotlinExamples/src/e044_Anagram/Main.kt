454f553317960ae0e6609784ec65f932711d17f9
Mert <mertturkmenoglu99@gmail.com>
2019-07-13 16:26:50 +0300

    Add anagram words example

---
+++ kotlinExamples/src/e044_Anagram/Main.kt
@@ -0,0 +1,7 @@
+package e044_Anagram
+
+fun isAnagram(first: String, second: String) = first.chars().sorted().toArray().zip(second.chars().sorted().toArray()).all { it.first == it.second }
+
+fun main() {
+    println(listOf("emily", "miley", "ylime", "mert").filter { isAnagram("emily", it) })
+}
