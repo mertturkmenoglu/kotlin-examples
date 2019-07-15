0d2784750082f4ba371498ccfe77851b67402111
Mert <mertturkmenoglu99@gmail.com>
2019-07-15 19:01:03 +0300

    Add word separate practice

---
+++ kotlinExamples/src/practice/e004_WordSeparate/Main.kt
@@ -0,0 +1,20 @@
+package practice.e004_WordSeparate
+
+val menu = listOf("Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke")
+
+fun getOrders(str: String): String {
+    var result = ""
+    var tmp = str
+
+    while (tmp.length > menu.minBy { it.length }!!.length) {
+        result += menu.find { tmp.startsWith(it.decapitalize()) }
+                .also { tmp = tmp.drop(it!!.length) }
+        result += ' '
+    }
+
+    return result.dropLast(1)
+}
+
+fun main() {
+    println(getOrders("milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"))
+}
