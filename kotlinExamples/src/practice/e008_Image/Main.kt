5b01f0fb736039f44b31e3e87e1dce79d066f8a7
Mert <mertturkmenoglu99@gmail.com>
2019-07-16 06:03:21 +0300

    Add image download example

---
+++ kotlinExamples/src/practice/e008_Image/Main.kt
@@ -0,0 +1,28 @@
+package practice.e008_Image
+
+import java.io.*
+import java.net.URL
+
+fun main() {
+    try {
+        val fileName = "Kotlin-logo.png"
+        val website = "https://upload.wikimedia.org/wikipedia/commons/b/b5/$fileName"
+        println("Downloading file from: $website")
+
+        val url = URL(website)
+        val inputStream = url.openStream()
+        val outputStream = FileOutputStream(fileName)
+        val buffer = Array<Byte>(2048) { 0 }.toByteArray()
+        var length = inputStream.read(buffer)
+
+        while (length != -1) {
+            outputStream.write(buffer, 0, length)
+            length = inputStream.read(buffer)
+        }
+
+        inputStream.close()
+        outputStream.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
