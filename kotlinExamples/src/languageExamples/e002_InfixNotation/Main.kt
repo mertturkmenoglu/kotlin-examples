ed68663ba912e052741e4f83002ae1f0e36edb34
Mert <mertturkmenoglu99@gmail.com>
2019-07-16 23:55:20 +0300

    Update infix notation example

--- kotlinExamples/src/languageExamples/e002_InfixNotation/Main.kt
+++ kotlinExamples/src/languageExamples/e002_InfixNotation/Main.kt
@@ -2,22 +2,30 @@ package languageExamples.e002_InfixNotation
 
 import kotlin.random.Random
 
+/**
+ * Example 002: Kotlin Infix Functions
+ *
+ * Demonstrating how infix functions work.
+ *
+ * @see Random
+ *
+ * Reference:
+ * https://kotlinlang.org/docs/reference/functions.html#infix-notation
+ */
 fun main() {
+    // Get random values for age and relationship status
     val age = Random.nextInt(10, 30)
     val isSingle = Random.nextBoolean()
 
+    // Here we us built-in type Boolean infix function or
     val result = (age < 18) or isSingle
     println("Age: $age\tisSingle: $isSingle")
     println(if (result) "Can't enter" else "Can enter")
 
+    // Now create our type objects
     val person1 = Bool(true)
     val person2 = Bool(false)
 
+    // We could call person1.or(person2)
     print(person1 or person2)
-}
-
-class Bool(var value: Boolean = false) {
-    infix fun or(other: Bool): Boolean {
-        return value or other.value
-    }
 }
