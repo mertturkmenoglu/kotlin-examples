5b5ac50e9106115e667e9fa536fb062aa9b7d520
Mert <mertturkmenoglu99@gmail.com>
2019-07-16 06:19:36 +0300

    Add image color read example

---
+++ kotlinExamples/src/practice/e009_Image/Main.kt
@@ -0,0 +1,38 @@
+package practice.e009_Image
+
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.io.*
+import javax.imageio.ImageIO
+
+class Pixel {
+    private lateinit var image: BufferedImage
+    private var width: Int = 0
+    private var height: Int = 0
+
+    init {
+        try {
+            val input = File(System.getProperty("user.dir") + "/kotlinExamples/anitkabir.jpeg")
+            image = ImageIO.read(input)
+            width = image.width
+            height = image.height
+
+            var count = 0
+
+            for (i in 0 until height) {
+                for (j in 0 until width) {
+                    count++
+                    val c = Color(image.getRGB(j, i))
+                    println("S.No: $count\tRed: ${c.red}\tGreen: ${c.green}\tBlue: ${c.blue}")
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+
+fun main() {
+    Pixel()
+}
