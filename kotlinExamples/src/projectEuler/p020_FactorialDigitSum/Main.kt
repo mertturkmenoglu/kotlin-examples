b9a5941cd4ae2b496a3dcfa1848caf70af3f49d4
Mert <mertturkmenoglu99@gmail.com>
2019-07-21 01:18:12 +0300

    Add Project Euler - P20 solution

---
+++ kotlinExamples/src/projectEuler/p020_FactorialDigitSum/Main.kt
@@ -0,0 +1,19 @@
+package projectEuler.p020_FactorialDigitSum
+
+import java.math.BigInteger
+
+/**
+ * n! means n × (n − 1) × ... × 3 × 2 × 1
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ */
+fun main() {
+    val result = factorialDigitSum(n = 100)
+    println(result)
+}
+
+fun factorialDigitSum(n: Int) = factorial(n).toString().map { it.toString().toInt() }.reduce { acc, i ->  acc + i}
+
+fun factorial(n: Int): BigInteger = if (n == 1) n.toBigInteger() else factorial(n-1) * n.toBigInteger()
