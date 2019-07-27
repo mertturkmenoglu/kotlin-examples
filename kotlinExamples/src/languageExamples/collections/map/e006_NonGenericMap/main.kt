c1655e54820c38ae53fd6a063d2afb8c8a1072d7
Mert <mertturkmenoglu99@gmail.com>
2019-07-27 18:48:39 +0300

    Add collection example
    Add non-generic map example

---
+++ kotlinExamples/src/languageExamples/collections/map/e006_NonGenericMap/main.kt
@@ -0,0 +1,9 @@
+package languageExamples.collections.map.e006_NonGenericMap
+
+fun main() {
+    val map = mapOf<Any, Any>("emily" to 1, 3.14 to "pi", Pair(1, 2) to 3.14f)
+
+    map.forEach {
+        println("${it.key.javaClass} - ${it.value.javaClass}")
+    }
+}
