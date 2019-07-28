27f7a0fb1e8a434d7e6c7f26b4c2f921c48a1cd6
Mert <mertturkmenoglu99@gmail.com>
2019-07-28 11:10:57 +0300

    Add collection example
    Add Set methods example

---
+++ kotlinExamples/src/languageExamples/collections/set/e001_SetMethods/Main.kt
@@ -0,0 +1,29 @@
+package languageExamples.collections.set.e001_SetMethods
+
+fun main() {
+    val set = setOf(1, 2, 3, 4)
+    println("set: $set")
+
+    println("set.contains(1): ${set.contains(1)}")
+    println("set.containsAll(listOf(2, 3)): ${set.containsAll(listOf(2, 3))}")
+
+    println("set.isEmpty(): ${set.isEmpty()}")
+    println("set.isNotEmpty(): ${set.isNotEmpty()}")
+    println("set.isNullOrEmpty(): ${set.isNullOrEmpty()}")
+
+    val positive = set.all { it > 0 }
+    println(positive)
+
+    val odd = set.any { it % 2 != 0 }
+    println(odd)
+
+    val count = set.count { it % 2 == 0 }
+    println(count)
+
+    for (element in set) {
+        println(element)
+    }
+
+    val newSet = set + 5
+    println(newSet)
+}
