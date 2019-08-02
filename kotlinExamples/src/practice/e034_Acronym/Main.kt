bc3ad0490e029dff9f5bcd0a3c5a01ae313bb9ee
Mert <mertturkmenoglu99@gmail.com>
2019-08-03 01:33:55 +0300

    Add acronym example

---
+++ kotlinExamples/src/practice/e034_Acronym/Main.kt
@@ -0,0 +1,10 @@
+package practice.e034_Acronym
+
+fun main() {
+    val input = "The Road _Not_ Taken"
+    val result = findAcronym(input)
+    println(result)
+}
+
+fun findAcronym(str: String) = str.filter { it.isWhitespace() || it.isLetter() }
+        .split("\\s+".toRegex()).fold("") { acc, s -> acc + s.first() }
