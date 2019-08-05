d535a9faadc5dbd9819add2df3c33f463c3b7a28
Mert <mertturkmenoglu99@gmail.com>
2019-08-05 23:02:51 +0300

    Add name to matrix practice

---
+++ kotlinExamples/src/practice/e048_NameMatrix/Main.kt
@@ -0,0 +1,20 @@
+package practice.e048_NameMatrix
+
+import kotlin.math.*
+
+fun main() {
+    val input = "emily"
+    val result = nameToMatrix(input)
+    result.forEach { println(it.asList()) }
+}
+
+fun nameToMatrix(input: String): Array<Array<Char>> {
+    require(input.isNotEmpty()) { "Name must be at least one letter" }
+
+    val n = ceil(sqrt(input.length.toDouble())).toInt()
+    val arr = Array(n) { Array(n) { '.' } }
+
+    input.forEachIndexed { i, e -> arr[i / n][i % n] = e }
+
+    return arr
+}
