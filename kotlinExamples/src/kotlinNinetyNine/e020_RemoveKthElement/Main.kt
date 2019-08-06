cfd2d44569323756f124e3e2d0252a1a701ac765
Mert <mertturkmenoglu99@gmail.com>
2019-08-06 13:24:49 +0300

    Add Kotlin - 99 Example
    Add remove k-th element example

---
+++ kotlinExamples/src/kotlinNinetyNine/e020_RemoveKthElement/Main.kt
@@ -0,0 +1,13 @@
+package kotlinNinetyNine.e020_RemoveKthElement
+
+fun main() {
+    val input = "abcd".toList()
+    val n = 1
+    val result = input.removeAtN(n)
+    println(result)
+}
+
+fun <T> List<T>.removeAtN(n: Int): Pair<List<T>, T> {
+    require(n >= 0) { "Invalid index" }
+    return this.toMutableList().apply { removeAt(n) } to this[n]
+}
