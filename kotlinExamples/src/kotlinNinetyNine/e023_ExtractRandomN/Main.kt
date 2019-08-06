d1b433c747d06e0f0bf55eb43ce7d325f6fd414e
Mert <mertturkmenoglu99@gmail.com>
2019-08-06 13:43:07 +0300

    Add Kotlin - 99 Example
    Add extract n random element example

---
+++ kotlinExamples/src/kotlinNinetyNine/e023_ExtractRandomN/Main.kt
@@ -0,0 +1,22 @@
+package kotlinNinetyNine.e023_ExtractRandomN
+
+fun main() {
+    val input = "abdcdefgh".toList()
+    val result = input.randomSelect(3)
+    println(result)
+}
+
+fun <T> List<T>.randomSelect(n: Int): List<T> {
+    require(n < this.size) { "Invalid number" }
+    val result = ArrayList<T>()
+
+    with(toMutableList()) {
+        repeat(n) {
+            val index = (0 until size).random()
+            result.add(this[index])
+            removeAt(index)
+        }
+    }
+
+    return result
+}
