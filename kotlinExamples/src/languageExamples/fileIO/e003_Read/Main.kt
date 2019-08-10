845122c85c40c1c8a26d634518453e664b7c7f7e
Mert <mertturkmenoglu99@gmail.com>
2019-08-10 12:26:24 +0300

    Add file input example

---
+++ kotlinExamples/src/languageExamples/fileIO/e003_Read/Main.kt
@@ -0,0 +1,10 @@
+package languageExamples.fileIO.e003_Read
+
+import java.io.File
+
+// Do not use with big files
+fun main() {
+    val file = System.getProperty("user.dir") + "/kotlinExamples/text/test.txt"
+    val allContent = File(file).readText()  // Read entire content as a single string
+    println(allContent)
+}
