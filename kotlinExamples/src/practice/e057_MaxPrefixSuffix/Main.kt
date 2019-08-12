05d6fc1f654ba634b6417b98804fe8d958c3704c
Mert <mertturkmenoglu99@gmail.com>
2019-08-12 15:14:01 +0300

    Add max prefix - suffix example

---
+++ kotlinExamples/src/practice/e057_MaxPrefixSuffix/Main.kt
@@ -0,0 +1,12 @@
+package practice.e057_MaxPrefixSuffix
+
+fun main() {
+    val input = "abcdabc"
+    val result = input.maxPrefixSuffix()
+    println(result)
+}
+
+fun String.maxPrefixSuffix() = sequence {
+    for (i in 0..length / 2)
+        if (take(i) == takeLast(i)) yield(i)
+}.max() ?: throw Exception()
