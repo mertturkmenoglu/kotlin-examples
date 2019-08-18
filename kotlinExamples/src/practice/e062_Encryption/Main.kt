e2240231c56544f5dfabb497d2b4cc59fb118865
Mert <mertturkmenoglu99@gmail.com>
2019-08-18 19:50:47 +0300

    Add string encryption practice

---
+++ kotlinExamples/src/practice/e062_Encryption/Main.kt
@@ -0,0 +1,16 @@
+package practice.e062_Encryption
+
+fun main() {
+    val input = "Emily"
+    val result = encrypt(input)
+    println(result)
+}
+
+fun encrypt(str: String) = str.map {
+    when (it) {
+        in ('A' until 'Z').union('a' until 'z') -> (it.toInt() + 1).toChar()
+        'Z' -> 'A'
+        'z' -> 'a'
+        else -> it
+    }
+}.joinToString("")
