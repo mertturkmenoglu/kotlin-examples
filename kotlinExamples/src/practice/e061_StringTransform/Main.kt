c5c19c7b31212e55c66af8d3b5f86fb18f2b6d3e
Mert <mertturkmenoglu99@gmail.com>
2019-08-18 19:38:56 +0300

    Add string transform practice

---
+++ kotlinExamples/src/practice/e061_StringTransform/Main.kt
@@ -0,0 +1,14 @@
+package practice.e061_StringTransform
+
+fun main() {
+    val input = "Emily is the BEST"
+    val result = input.transform()
+    println(result)
+}
+
+fun String.transform(): String {
+    assert(isNotEmpty())
+    return filter { it.isLetter() || it.isWhitespace() }.split(" ").reversed().joinToString(" ") {
+        it.map { c -> if (c.isLowerCase()) c.toUpperCase() else c.toLowerCase() }.joinToString("")
+    }
+}
