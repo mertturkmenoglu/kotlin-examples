51030534d684c1f087c2d99c81a5dc1ccd7cc0b8
Mert <mertturkmenoglu99@gmail.com>
2019-08-19 17:25:12 +0300

    Add coroutine example

---
+++ kotlinExamples/src/languageExamples/coroutines/e004_/Main.kt
@@ -0,0 +1,19 @@
+package languageExamples.coroutines.e004_
+
+import kotlinx.coroutines.*
+import java.time.LocalTime
+
+fun main() {
+    runBlocking {
+        println("Start of program: ${LocalTime.now()}")
+
+        GlobalScope.launch {
+            delay(10_000L)
+            println("Inside launch after delay: ${LocalTime.now()}")    // Will not hit this point
+        }
+
+        println("After launch: ${LocalTime.now()}")
+        delay(2_000L)
+        println("End of program: ${LocalTime.now()}")
+    }
+}
