0f989545bb97fe09ca11cb49f1c7ccd90fa46811
Mert <mertturkmenoglu99@gmail.com>
2019-08-19 15:03:35 +0300

    Add JSON example

---
+++ kotlinExamples/src/languageExamples/networking/e004_JSONParse/Main.kt
@@ -0,0 +1,27 @@
+package languageExamples.networking.e004_JSONParse
+
+import com.beust.klaxon.Klaxon
+import java.net.HttpURLConnection
+import java.net.URL
+
+data class Repository(val id: Number, val node_id: String, val name: String, val full_name: String)
+
+fun main() {
+    println("Enter your GitHub username: ")
+    val username = readLine() ?: throw Exception()
+    val url = URL("https://api.github.com/users/$username/repos")
+
+    val jsonResponse = with(url.openConnection() as     HttpURLConnection) {
+        requestMethod = "GET"
+
+        val strBuilder = StringBuilder()
+        inputStream.bufferedReader().use { it.lines().forEach { line -> strBuilder.append(line) } }
+
+        strBuilder.toString()
+    }
+
+    val klaxon = Klaxon()
+    val repos = klaxon.parseArray<Repository>(jsonResponse)
+
+    repos?.forEach { println(it.name) }
+}
