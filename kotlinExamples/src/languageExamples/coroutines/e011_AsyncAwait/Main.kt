fbb9fd62b41545bfa2776056ad3c2adc3bf73bf1
Mert <mertturkmenoglu99@gmail.com>
2019-08-19 20:06:31 +0300

    Add coroutine example

---
+++ kotlinExamples/src/languageExamples/coroutines/e011_AsyncAwait/Main.kt
@@ -0,0 +1,24 @@
+package languageExamples.coroutines.e011_AsyncAwait
+
+import com.beust.klaxon.Klaxon
+import kotlinx.coroutines.*
+import java.net.URL
+
+data class Repository(val id: Number, val node_id: String, val name: String, val full_name: String)
+
+fun main() {
+    println("Enter your GitHub username: ")
+    val userName = readLine() ?: throw Exception()
+
+    val jsonResponse = GlobalScope.async { URL("https://api.github.com/users/$userName/repos").readText() }
+
+    println("It will continue")
+
+    runBlocking {
+        val klaxon = Klaxon()
+        println("Before parse")
+        val repos = klaxon.parseArray<Repository>(jsonResponse.await())
+        println("After parse\n\n")
+        repos?.forEach { println(it.name) }
+    }
+}
