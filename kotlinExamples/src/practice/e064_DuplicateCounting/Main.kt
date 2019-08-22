c25a797e7f23bef4cd2d4a7c22e96c1021a9422a
Mert <mertturkmenoglu99@gmail.com>
2019-08-22 16:33:47 +0300

    Add duplicate char count practice

---
+++ kotlinExamples/src/practice/e064_DuplicateCounting/Main.kt
@@ -0,0 +1,18 @@
+package practice.e064_DuplicateCounting
+
+fun main() {
+    val input = "Indivisibilities"
+    val result = duplicateCount(input)
+    println(result)
+}
+
+fun duplicateCount(str: String): String {
+    val duplicates =  str.groupingBy { it }.eachCount().filter { it.value > 1 }
+
+    val strBuilder = StringBuilder()
+    for ((k, v) in duplicates) {
+        strBuilder.append("'$k' occurs $v times\n")
+    }
+
+    return strBuilder.dropLast(1).toString()
+}
