16f66d6d46107804d5b5f278ce90d9e8955dc93d
Mert <mertturkmenoglu99@gmail.com>
2019-08-25 03:47:13 +0300

    Add file example

---
+++ kotlinExamples/src/languageExamples/fileIO/e009_Walk/Main.kt
@@ -0,0 +1,10 @@
+package languageExamples.fileIO.e009_Walk
+
+import java.io.File
+
+fun main() {
+    println("Please enter the path: ")
+    val path = readLine() ?: throw Exception()
+
+    File(path).walk().forEach(::println)
+}
