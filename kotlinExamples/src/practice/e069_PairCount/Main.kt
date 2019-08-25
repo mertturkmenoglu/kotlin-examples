8ef5473b6afce392fb9c3430127d6bc9833192cd
Mert <mertturkmenoglu99@gmail.com>
2019-08-25 11:35:28 +0300

    Add pair count practice

---
+++ kotlinExamples/src/practice/e069_PairCount/Main.kt
@@ -0,0 +1,9 @@
+package practice.e069_PairCount
+
+fun main() {
+    val input = listOf(1, 1, 2, 1, 2, 3, 1, 1, 2, 3, 1)
+    val result = pairCount(input)
+    println(result)
+}
+
+fun <T> pairCount(l: List<T>) = l.groupingBy { it }.eachCount().values.fold(0) { acc, i ->  acc + i / 2 }
