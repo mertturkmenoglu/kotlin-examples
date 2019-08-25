3307b6468c7adb91609107756c2e578d74930303
Mert <mertturkmenoglu99@gmail.com>
2019-08-25 09:38:54 +0300

    Add accumulate practice

---
+++ kotlinExamples/src/practice/e067_Accumulate/Main.kt
@@ -0,0 +1,14 @@
+package practice.e067_Accumulate
+
+fun main() {
+    val input = "RqaEzty"
+    val result = accumulate(input)
+    println(result)
+}
+
+fun accumulate(input: String):String {
+    require(input.all { it in 'a' .. 'z' union 'A' .. 'Z' })
+    return input.toLowerCase().mapIndexed { i, c ->
+        c.toUpperCase() + c.toString().repeat(i)
+    }.joinToString("-")
+}
