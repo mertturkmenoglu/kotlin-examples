14f3679bcdb009faa8fc8cd853f2b30a4d4f566f
Mert <mertturkmenoglu99@gmail.com>
2019-08-25 04:29:07 +0300

    Add balanced parentheses practice

---
+++ kotlinExamples/src/practice/e065_BalancedParentheses/Main.kt
@@ -0,0 +1,28 @@
+package practice.e065_BalancedParentheses
+
+import java.util.*
+
+fun main() {
+    val input = "[(3x+5) + 7 * (5x + 1)] + 2 * 7"
+    val result = isBalanced(input)
+    println(result)
+}
+
+fun isBalanced(str: String): Boolean {
+    val parentheses = "{[()]}"
+    val inp = str.filter { it in parentheses }.apply { if (length % 2 == 1) return false }
+
+    val s = Stack<Char>()
+
+    for (p in inp) {
+        if (p in parentheses.slice(0..2)) {
+            s.push(p)
+        } else {
+            if (s.isEmpty() || (parentheses.indexOf(s.peek()) != (parentheses.length - 1 - parentheses.indexOf(p))))
+                return false
+            s.pop()
+        }
+    }
+
+    return true
+}
