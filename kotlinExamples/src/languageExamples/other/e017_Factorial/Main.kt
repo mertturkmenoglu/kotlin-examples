d89d4cdbcd160ad0dede3b4a147308fcd728771b
Mert <mertturkmenoglu99@gmail.com>
2019-08-26 19:29:11 +0300

    Add tail recursion example

---
+++ kotlinExamples/src/languageExamples/other/e017_Factorial/Main.kt
@@ -0,0 +1,22 @@
+package languageExamples.other.e017_Factorial
+
+fun main() {
+    val input = 17
+    val result1 = factorial(input.toLong())
+    val result2 = oldFactorial(input)
+
+    println(result1)
+    println(result2)
+}
+
+// Tail recursion is a concept comes from functional programming
+// It gives a hint to compiler for stack usage optimisation
+tailrec fun factorial(n: Long, r: Long = 1L): Long {
+    return if (n < 1L) r else factorial(n-1, r * n)
+}
+
+// Good old recursive function.
+// Keep an eye on stack overflows
+fun oldFactorial(n: Int): Long {
+    return if (n < 1) 1 else n.toLong() * oldFactorial(n-1)
+}
