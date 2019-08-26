72449e6680cb077c6376e29016ede7bb354c94a3
Mert <mertturkmenoglu99@gmail.com>
2019-08-26 23:15:15 +0300

    Add time measurement example

---
+++ kotlinExamples/src/languageExamples/other/e018_TimeMeasurement/Main.kt
@@ -0,0 +1,14 @@
+package languageExamples.other.e018_TimeMeasurement
+
+import kotlin.system.measureNanoTime
+
+fun main() {
+    val elapsedTime = measureNanoTime { heavyCalculation() }
+    println("${elapsedTime / 1_000_000F} sec")
+}
+
+fun heavyCalculation() {
+    for (i in 0 .. 1_000_000) {
+        kotlin.math.sqrt(i.toDouble())
+    }
+}
