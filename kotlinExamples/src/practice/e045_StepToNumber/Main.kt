bf18a7803448b0e7e6b771751cfb1ce82a9b3cdb
Mert <mertturkmenoglu99@gmail.com>
2019-08-27 12:48:49 +0300

    Add description for step to number example

--- kotlinExamples/src/practice/e045_StepToNumber/Main.kt
+++ kotlinExamples/src/practice/e045_StepToNumber/Main.kt
@@ -1,5 +1,11 @@
 package practice.e045_StepToNumber
 
+/**
+ * Find how many steps take to reach a number:
+ * - Start from 1
+ * - You can multiply by two or increase by one
+ * - Find the shortest way to reach the given number
+ */
 fun main() {
     val n = 12
     val result = steps(n)
