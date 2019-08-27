7ff9c62bdce9795e43bdfc52cfa6e7ed16fce141
Mert <mertturkmenoglu99@gmail.com>
2019-08-27 14:02:16 +0300

    Update comment strip example
    Change function name and add unit tests

--- kotlinExamples/src/practice/e072_CommentStrip/Main.kt
+++ kotlinExamples/src/practice/e072_CommentStrip/Main.kt
@@ -2,11 +2,11 @@ package practice.e072_CommentStrip
 
 fun main() {
     val input = "apples, plums % and bananas\npears\noranges !applesauce"
-    val result = solution(input, charArrayOf('%', '!'))
+    val result = comment(input, charArrayOf('%', '!'))
     println(result)
 }
 
-fun solution(input: String, c: CharArray): String {
+fun comment(input: String, c: CharArray): String {
     return input.split('\n').joinToString("\n") {
         it.takeWhile { ch -> ch !in c }.trim()
     }
