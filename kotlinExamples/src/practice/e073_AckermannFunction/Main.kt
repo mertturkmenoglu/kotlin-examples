9e6dd4a09e9372c5711759b0dec9b2077da9ec61
Mert <mertturkmenoglu99@gmail.com>
2019-08-28 17:54:25 +0300

    Add Ackermann Function example

---
+++ kotlinExamples/src/practice/e073_AckermannFunction/Main.kt
@@ -0,0 +1,18 @@
+package practice.e073_AckermannFunction
+
+fun main() {
+    try {
+        val result = ack(3, 12)
+        println(result)
+    } catch (e: StackOverflowError) {
+       e.printStackTrace()
+    }
+}
+
+fun ack(m: Int, n: Int): Int {
+    return when {
+        m == 0 -> n + 1
+        n == 0 -> ack(m-1, 1)
+        else -> ack(m-1, ack(m, n-1))
+    }
+}
