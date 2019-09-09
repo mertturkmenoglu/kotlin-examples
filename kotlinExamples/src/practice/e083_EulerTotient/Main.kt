b6b45143c7bb20e423893e24eedfeb27cfd268c9
Mert <mertturkmenoglu99@gmail.com>
2019-09-09 20:52:41 +0300

    Add euler totient function practice

---
+++ kotlinExamples/src/practice/e083_EulerTotient/Main.kt
@@ -0,0 +1,24 @@
+package practice.e083_EulerTotient
+
+fun main() {
+    val input = 9_999_999_985L
+    val result = totient(input)
+    println(result)
+}
+
+fun totient(n: Long): Int{
+    require(n >= 1L) { "Function is valid for n >= 1" }
+    return (1L .. n).count { gcd(n, it) == 1L }
+}
+
+fun gcd(a: Long, b: Long): Long {
+    var max = 0L
+    val limit = if (a < b) a else b
+
+    for (i in 1 .. limit) {
+        if (a % i == 0L && b % i == 0L)
+            max = i
+    }
+
+    return max
+}
---
+++ kotlinExamples/src/practice/e083_EulerTotient/README.md
@@ -0,0 +1,12 @@
+# Euler Totient Function
+***
+* Original problem description: https://www.codewars.com/kata/53c9157c689f841d16000c03
+* Function counts positive integer numbers such that <= to n and that
+are relatively prime to n.
+* Example:
+    ```
+    n = 9
+    1 <= k <= 9 --> 1, 2, 4, 5, 7, 8, 9 
+    ```
+* f(9) = 6 and f(1) = 1.
+* Function should take an integer **1 <= 9** and return **f(n)**
