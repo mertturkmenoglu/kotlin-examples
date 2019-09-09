de5373ff4069f49cb6cd0f27dda87ca89dba0349
Mert <mertturkmenoglu99@gmail.com>
2019-09-09 16:35:02 +0300

    Add ticket sale practice

---
+++ kotlinExamples/src/practice/e080_CinemaLine/Main.kt
@@ -0,0 +1,23 @@
+package practice.e080_CinemaLine
+
+fun main() {
+    val input = listOf(25, 25, 50, 50, 100)
+    val result = tickets(input)
+    println(result)
+}
+
+fun tickets(line: List<Int>): Boolean {
+    var register = 0
+
+    for (bill in line) {
+        register += 25
+
+        if (bill != 25)
+            register -= bill
+
+        if (register < 0)
+            return false
+    }
+
+    return true
+}
---
+++ kotlinExamples/src/practice/e080_CinemaLine/README.md
@@ -0,0 +1,7 @@
+# Cinema Line
+***
+* Each person has a single bill, **100**, **50** or **25**.
+* Each ticket is **25$**.
+* **Initially**, cash register is **empty**.
+* If cashier can sell ticket to every person, than return **true** else **false**.
+* You **have to** sell tickets **according to queue**.
