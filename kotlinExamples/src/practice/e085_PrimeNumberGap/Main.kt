eae8b9613fba028afcc0b6ea8432031ba1287634
Mert <mertturkmenoglu99@gmail.com>
2019-09-12 16:52:45 +0300

    Add prime number pair practice

---
+++ kotlinExamples/src/practice/e085_PrimeNumberGap/Main.kt
@@ -0,0 +1,23 @@
+package practice.e085_PrimeNumberGap
+
+import kotlin.math.sqrt
+
+fun main() {
+    val gap = 4
+    val start = 130
+    val end = 200
+    val result = primeNumberPairWithGap(start, end, gap)
+    println(result)
+}
+
+fun primeNumberPairWithGap(start: Int, end: Int, gap: Int): Pair<Int, Int>? {
+    return (start .. end).filter(Int::isPrime).zipWithNext().first { it.first == it.second + gap }
+}
+
+fun Int.isPrime() = when {
+    this < 2 -> false
+    this == 2 -> true
+    this % 2 == 0 -> false
+    (3 .. sqrt(this.toDouble()).toInt()).any { this % it == 0 } -> false
+    else -> true
+}
---
+++ kotlinExamples/src/practice/e085_PrimeNumberGap/README.md
@@ -0,0 +1,4 @@
+# Prime Number Gap
+***
+* Original problem description: https://www.codewars.com/kata/gap-in-primes
+* Function takes a range and a gap value and returns the first prime pair spaced with a gap of **g** in the range [**m**, **n**] otherwise **null**.
