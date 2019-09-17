428ea790a02998f19e25fb9328d4518675e416a7
Mert <mertturkmenoglu99@gmail.com>
2019-09-17 23:41:41 +0300

    Add date time formatting example

---
+++ kotlinExamples/src/languageExamples/datetime/e002_Formatter/Main.kt
@@ -0,0 +1,13 @@
+package languageExamples.datetime.e002_Formatter
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun main() {
+    val dateString = "September 13, 2042"
+    val formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale.ENGLISH)
+    val dateObject = LocalDate.parse(dateString, formatter)
+
+    println(dateObject)
+}
