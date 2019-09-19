054ba66739eff14a5dfa1fd73ef7e1ba84297f29
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 20:24:05 +0300

    Add reflection example

---
+++ kotlinExamples/src/languageExamples/reflection/e002_KClass/Main.kt
@@ -0,0 +1,21 @@
+package languageExamples.reflection.e002_KClass
+
+import java.io.Serializable
+import kotlin.reflect.full.superclasses
+import kotlin.reflect.jvm.jvmName
+
+class TestClass : Serializable
+
+fun main() {
+    val type = TestClass::class
+
+    println("Type: $type")
+    println("Superclasses: ${type.superclasses}")
+    println("Is final: ${type.isFinal}")
+    println("Simple name: ${type.simpleName}")
+    println("Type parameters: ${type.typeParameters}")
+    println("Annotations: ${type.annotations}")
+    println("Members: ${type.members}")
+    println("Java object type: ${type.javaObjectType}")
+    println("JVM name: ${type.jvmName}")
+}
