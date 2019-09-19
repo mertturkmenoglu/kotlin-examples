0394555a9756dc671cedfc7928bc3cd3f784c1cb
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 19:32:20 +0300

    Update DSL example
    Add new infix function example

--- kotlinExamples/src/languageExamples/dsl/e001_InfixFunction/Main.kt
+++ kotlinExamples/src/languageExamples/dsl/e001_InfixFunction/Main.kt
@@ -3,4 +3,8 @@ package languageExamples.dsl.e001_InfixFunction
 fun main() {
     "Emily" shouldBeEqual "emily".capitalize()
     10 shouldBeEqual 5
+
+    val name = "Emily"
+    val surname = " Smith"
+    println(name concat surname)
 }
