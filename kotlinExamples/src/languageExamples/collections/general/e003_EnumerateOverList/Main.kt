d3a3be07ba281ac3ce6da213f5cc8d35cc4b3964
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 19:37:43 +0300

    Add list enumeration example

---
+++ kotlinExamples/src/languageExamples/collections/general/e003_EnumerateOverList/Main.kt
@@ -0,0 +1,25 @@
+package languageExamples.collections.general.e003_EnumerateOverList
+
+fun main() {
+    val list = listOf("Emily", "Diana", "Barbara", "Selina")
+
+    // Iterating like C style
+    for (i in 0 until list.size) {
+        println("Index: $i Value: ${list[i]}")
+    }
+
+    println("----")
+
+    // For-each loop style with a helper index variable
+    var index = 0
+    for (e in list) {
+        println("Index: ${index++} Value: $e")
+    }
+
+    println("----")
+
+    // Enumeration
+    for ((i, e) in list.withIndex()) {
+        println("Index: $i Value: $e")
+    }
+}
