717e7ff542792775a440b59b303efc5dddf0a957
Mert <mertturkmenoglu99@gmail.com>
2019-09-19 20:24:19 +0300

    Add reflection example

---
+++ kotlinExamples/src/languageExamples/reflection/e003_KClass/Main.kt
@@ -0,0 +1,16 @@
+package languageExamples.reflection.e003_KClass
+
+import kotlin.reflect.full.*
+
+class Vector2D(private val x: Int, private val y: Int) {
+    operator fun plus(other: Vector2D) = Vector2D(x + other.x, y + other.y)
+}
+
+fun main() {
+    val type = Vector2D::class
+
+    println("Type parameters: ${type.typeParameters}")
+    println("Members: ${type.members}")
+    println("Member properties: ${type.memberProperties}")
+    println("Member functions: ${type.memberFunctions}")
+}
