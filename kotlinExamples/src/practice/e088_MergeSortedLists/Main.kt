c9d861e6a41b713a920f070786dd775875305b6a
Mert <mertturkmenoglu99@gmail.com>
2019-09-21 08:24:32 +0300

    Update merge sorted lists example
    Add recursive merge example

--- kotlinExamples/src/practice/e088_MergeSortedLists/Main.kt
+++ kotlinExamples/src/practice/e088_MergeSortedLists/Main.kt
@@ -8,6 +8,7 @@ fun main() {
     println(fst)
     println(snd)
     println(result)
+    println(recMerge(fst, snd))
 }
 
 fun <T: Comparable<T>> merge(fst: List<T>, snd: List<T>): List<T> {
@@ -22,4 +23,16 @@ fun <T: Comparable<T>> merge(fst: List<T>, snd: List<T>): List<T> {
     result.addAll(fst.drop(i) + snd.drop(j))
 
     return result
+}
+
+tailrec fun <T: Comparable<T>> recMerge(fst: List<T>, snd: List<T>, result: ArrayList<T> = arrayListOf()): List<T> {
+    return when {
+        fst.isEmpty() && snd.isEmpty() -> result
+        fst.isEmpty() -> recMerge(fst, snd.drop(1), result.apply { add(snd.first()) })
+        snd.isEmpty() -> recMerge(fst.drop(1), snd, result.apply { add(fst.first()) })
+        else -> when {
+            fst.first() <= snd.first() -> recMerge(fst.drop(1), snd, result.apply { add(fst.first()) })
+            else -> recMerge(fst, snd.drop(1), result.apply { add(snd.first()) })
+        }
+    }
 }
