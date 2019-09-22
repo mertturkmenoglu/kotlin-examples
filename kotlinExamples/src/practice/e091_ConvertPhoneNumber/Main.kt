fde89d24a065c45774073df6c9e90fa5261cc9ce
Mert <mertturkmenoglu99@gmail.com>
2019-09-23 00:44:45 +0300

    Add phone number conversion practice

---
+++ kotlinExamples/src/practice/e091_ConvertPhoneNumber/Main.kt
@@ -0,0 +1,12 @@
+package practice.e091_ConvertPhoneNumber
+
+fun main() {
+    val input = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    val result = convertPhoneNumber(input)
+    println(result)
+}
+
+fun convertPhoneNumber(l: List<Int>): String {
+    return "(" + l.take(3).joinToString("") + ") " + l.subList(3, 6).joinToString("") + "-" + l.takeLast(4).joinToString("")
+}
+
