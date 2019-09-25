7fd27c801c475fafcc9e9d451f0b88f635829662
Mert <mertturkmenoglu99@gmail.com>
2019-09-26 01:46:15 +0300

    Update matrix rotate example

--- kotlinExamples/src/practice/e020_MatrixRotate/Main.kt
+++ kotlinExamples/src/practice/e020_MatrixRotate/Main.kt
@@ -1,63 +1,38 @@
 package practice.e020_MatrixRotate
 
 fun main() {
-
     println("Please enter row number: ")
-    val row = readLine()!!.toInt()
+    val row = readLine()?.toInt() ?: throw Exception()
+
     println("Please enter column number: ")
-    val col = readLine()!!.toInt()
+    val col = readLine()?.toInt() ?: throw Exception()
 
-    val mtr = Array(row) { Array(col) { 0 } }
-    for (i in 0 until row) {
-        for (j in 0 until col) {
-            mtr[i][j] = i * col + j
-        }
-    }
+    val mtr = MutableList(row) { i -> MutableList(col) { j -> i * col + j } }
 
-    for (Row in mtr) {
-        println(Row.asList())
-    }
-
+    mtr.forEach(::println)
     rotateX(mtr)
-
-    for (Row in mtr) {
-        println(Row.asList())
-    }
+    mtr.forEach(::println)
 
     // Revert to original
     rotateX(mtr)
 
-    // --------------
-
-    for (Row in mtr) {
-        println(Row.asList())
-    }
-
+    mtr.forEach(::println)
     rotateY(mtr)
-
-    for (Row in mtr) {
-        println(Row.asList())
-    }
+    mtr.forEach(::println)
 }
 
-fun rotateY(mtr: Array<Array<Int>>) {
-    val row = mtr.size
-    val col = mtr[0].size
-
-    for (i in 0 until row) {
-        for (j in 0 until (col / 2)) {
-            mtr[i][j] = mtr[i][col - j - 1].also { mtr[i][col - j - 1] = mtr[i][j] }
+fun rotateY(mtr: MutableList<MutableList<Int>>) {
+    for (i in mtr.indices) {
+        for (j in 0 until mtr.first().size / 2) {
+            mtr[i][j] = mtr[i][mtr.first().size - j - 1].also { mtr[i][mtr.first().size - j - 1] = mtr[i][j] }
         }
     }
 }
 
-fun rotateX(mtr: Array<Array<Int>>) {
-    val row = mtr.size
-    val col = mtr[0].size
-
-    for (i in 0 until (row / 2)) {
-        for (j in 0 until col) {
-            mtr[i][j] = mtr[row - i - 1][j].also { mtr[row - i -1][j] = mtr[i][j] }
+fun rotateX(mtr: MutableList<MutableList<Int>>) {
+    for (i in 0 until mtr.size / 2) {
+        for (j in mtr.first().indices) {
+            mtr[i][j] = mtr[mtr.size - i - 1][j].also { mtr[mtr.size - i - 1][j] = mtr[i][j] }
         }
     }
 }
