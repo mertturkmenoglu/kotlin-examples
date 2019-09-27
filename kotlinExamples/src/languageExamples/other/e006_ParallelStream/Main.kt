d80cdf6e851ef8e1153f2565210d162a6039a0e8
Mert <mertturkmenoglu99@gmail.com>
2019-09-27 19:34:13 +0300

    Add parallel stream example

---
+++ kotlinExamples/src/languageExamples/other/e006_ParallelStream/Main.kt
@@ -0,0 +1,10 @@
+package languageExamples.other.e006_ParallelStream
+
+fun main() {
+    listOf(2, 3, 5, 7, 11, 13, 17).parallelStream().forEach(::showNumber)
+}
+
+fun showNumber(n: Int) {
+    Thread.sleep(500)
+    println(n)
+}
