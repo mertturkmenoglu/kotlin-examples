135d4f447166c4bf2bbc76c8a71ca0daf6f3d22c
Mert <mertturkmenoglu99@gmail.com>
2019-09-27 20:16:59 +0300

    Add file IO example

---
+++ kotlinExamples/src/languageExamples/fileIO/e017_IO/Main.kt
@@ -0,0 +1,22 @@
+package languageExamples.fileIO.e017_IO
+
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
+
+fun main() {
+    try {
+        val outputPath = input("Output path: ")
+
+        ObjectOutputStream(FileOutputStream(outputPath)).use {
+            it.writeObject(arrayOf(1, 2, 3))
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun input(message: String): String {
+    println(message)
+
+    return readLine() ?: throw Exception()
+}
