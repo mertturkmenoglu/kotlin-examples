4c430958d2a187a6247cf6cbe75b88f71c9c6de3
Mert <mertturkmenoglu99@gmail.com>
2019-09-27 20:00:05 +0300

    Add mkdir() example

---
+++ kotlinExamples/src/languageExamples/fileIO/e014_Mkdir/Main.kt
@@ -0,0 +1,19 @@
+package languageExamples.fileIO.e014_Mkdir
+
+import java.io.File
+
+fun main() {
+    val path = input("Enter your path: ")
+
+    with(File(path)) {
+        if (!exists()) {
+            mkdir()
+        }
+    }
+}
+
+fun input(message: String): String {
+    println(message)
+
+    return readLine() ?: throw Exception()
+}
