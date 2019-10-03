41ecf011157922026de6e4411e39f35f2251520e
Mert <mertturkmenoglu99@gmail.com>
2019-10-03 21:09:02 +0300

    Add database connection example

---
+++ kotlinExamples/src/languageExamples/db/e001_SQLite/Main.kt
@@ -0,0 +1,25 @@
+package languageExamples.db.e001_SQLite
+
+import java.sql.DriverManager
+import java.sql.SQLException
+
+fun main() {
+    Connect.connect()
+}
+
+class Connect {
+    companion object {
+        @JvmStatic
+        fun connect() {
+            val url = "jdbc:sqlite:${System.getProperty("user.dir")}/test.db"
+
+            try {
+                val connection = DriverManager.getConnection(url)
+                println("Connection established")
+                connection.close()
+            } catch (e: SQLException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
