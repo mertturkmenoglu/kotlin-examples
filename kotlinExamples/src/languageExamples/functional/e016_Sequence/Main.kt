10044653bbe5c3cb055c846ac57caf7d736fe882
Mert <mertturkmenoglu99@gmail.com>
2019-10-04 19:30:08 +0300

    Add sequence block example

---
+++ kotlinExamples/src/languageExamples/functional/e016_Sequence/Main.kt
@@ -0,0 +1,15 @@
+package languageExamples.functional.e016_Sequence
+
+fun main() {
+    val lazySequence = sequence {
+        var i = 0
+        yieldAll(i .. i + 5)
+
+        while (true) {
+            i+=6
+            yieldAll(i .. i + 5)
+        }
+    }
+
+    lazySequence.take(20).forEach(::println)
+}
