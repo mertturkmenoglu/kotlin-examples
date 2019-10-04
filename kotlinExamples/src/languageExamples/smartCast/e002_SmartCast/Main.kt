e1f253892dbb5de2e2aa79be8b7ab053f8710a66
Mert <mertturkmenoglu99@gmail.com>
2019-10-04 19:54:07 +0300

    Add smart cast example

---
+++ kotlinExamples/src/languageExamples/smartCast/e002_SmartCast/Main.kt
@@ -0,0 +1,16 @@
+package languageExamples.smartCast.e002_SmartCast
+
+fun main() {
+    smart(3)
+    smart("Emily")
+}
+
+fun smart(x: Any) {
+    if (x is String) {
+        // Smart cast
+        println(x)
+        println(x.length)
+    } else {
+        println("$x is not a string")
+    }
+}
