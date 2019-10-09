8a4fc274934f22073032d81f37ba6f0806a8b99c
Mert <mertturkmenoglu99@gmail.com>
2019-10-10 00:18:59 +0300

    Add bogo sort practice

---
+++ kotlinExamples/src/practice/e098_BogoSort/Main.kt
@@ -0,0 +1,15 @@
+package practice.e098_BogoSort
+
+import kotlin.random.Random
+
+fun main() {
+    val input = mutableListOf<Int>().apply { repeat(Random.nextInt(3, 5)) { add(Random.nextInt(-50, 50)) } }
+    val result = bogoSort(input)
+
+    println(input)
+    println(result)
+}
+
+tailrec fun bogoSort(l: List<Int>): List<Int> =  if (isSorted(l)) l else bogoSort(l.shuffled())
+
+fun isSorted(l: List<Int>) = l.zipWithNext().all { it.first < it.second }
