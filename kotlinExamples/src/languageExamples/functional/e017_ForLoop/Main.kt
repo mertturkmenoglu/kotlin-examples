d407db805bf2a4c87168a2e683c6acc87669b636
Mert <mertturkmenoglu99@gmail.com>
2019-10-11 00:52:09 +0300

    Add higher order function example

---
+++ kotlinExamples/src/languageExamples/functional/e017_ForLoop/Main.kt
@@ -0,0 +1,11 @@
+package languageExamples.functional.e017_ForLoop
+
+fun main() {
+    (0..10).forLoopIndexed(::println)
+}
+
+private inline fun IntRange.forLoopIndexed(f: (Int) -> Unit) {
+    for (i in this) {
+        f(i)
+    }
+}
