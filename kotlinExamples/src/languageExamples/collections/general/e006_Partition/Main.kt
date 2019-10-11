45056c4976d113eb91d433661ca400854e7a1fd7
Mert <mertturkmenoglu99@gmail.com>
2019-10-11 16:35:25 +0300

    Add partition example

---
+++ kotlinExamples/src/languageExamples/collections/general/e006_Partition/Main.kt
@@ -0,0 +1,12 @@
+package languageExamples.collections.general.e006_Partition
+
+import kotlin.random.Random
+
+fun main() {
+    val list = mutableListOf<Int>().apply { repeat(Random.nextInt(5, 10)) { add(Random.nextInt(50)) } }
+    val (fst, snd) = list.partition { it % 2 == 0 }
+
+    println(list)
+    println(fst)
+    println(snd)
+}
