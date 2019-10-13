71616d02d2b7c00ce684e2aa5a6318cbd90351d1
Mert <mertturkmenoglu99@gmail.com>
2019-10-13 16:57:29 +0300

    Add string subpattern practice

---
+++ kotlinExamples/src/practice/e100_StringSubpattern/Main.kt
@@ -0,0 +1,20 @@
+@file:Suppress("SpellCheckingInspection")
+
+package practice.e100_StringSubpattern
+
+fun main() {
+    val inputs = listOf("a", "aaaa", "abcd", "abababab", "ababababa")
+    val results = inputs.map(::hasSubpattern)
+    println(results)
+}
+
+fun hasSubpattern(str: String): Boolean {
+    require(str.isNotEmpty()) { "Input string can not be empty" }
+
+    for (i in 1 until str.length) {
+        if (mutableListOf("").apply { repeat(str.length / i) { add(str.take(i)) } }.joinToString("") == str)
+            return true
+    }
+
+    return false
+}
