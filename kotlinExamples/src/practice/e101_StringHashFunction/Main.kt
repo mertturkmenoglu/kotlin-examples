d31b822a2d95e6177ecc2b0b2d9328e979204ca5
Mert <mertturkmenoglu99@gmail.com>
2019-10-16 21:35:28 +0300

    Add string hash function practice

---
+++ kotlinExamples/src/practice/e101_StringHashFunction/Main.kt
@@ -0,0 +1,14 @@
+package practice.e101_StringHashFunction
+
+import kotlin.math.pow
+
+fun main() {
+    val inputs = listOf("male", "lame", "elma")
+    val results = inputs.map { stringHash(it, r = 31, m = 47) }
+    println(results)
+}
+
+fun stringHash(str: String, r: Int = 31, m: Int = 97): Int {
+    return str.map { it - 'a' + 1 }
+            .foldIndexed(1F) { i: Int, acc: Float, e: Int -> acc + e * r.toFloat().pow(i) }.toInt().rem(m)
+}
