5c465396af8e51dd68215c616eb1aab66c3198cf
Mert <mertturkmenoglu99@gmail.com>
2019-10-24 04:38:11 +0300

    Add design pattern example
    Add adapter design pattern example

---
+++ kotlinExamples/src/languageExamples/designPatterns/e001_Adapter/Length.kt
@@ -0,0 +1,5 @@
+package languageExamples.designPatterns.e001_Adapter
+
+interface Length {
+    var length: Double
+}
---
+++ kotlinExamples/src/languageExamples/designPatterns/e001_Adapter/MetricLength.kt
@@ -0,0 +1,3 @@
+package languageExamples.designPatterns.e001_Adapter
+
+class MetricLength(override var length: Double) : Length
