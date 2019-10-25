506b85503e9506a15b1af79320c83e6010947137
Mert <mertturkmenoglu99@gmail.com>
2019-10-25 19:23:51 +0300

    Add Linear Search example

---
+++ kotlinExamples/src/languageExamples/algorithms/search/e001_LinearSearch/Main.kt
@@ -0,0 +1,19 @@
+package languageExamples.algorithms.search.e001_LinearSearch
+
+fun main() {
+    val list = listOf(9, 2, 1, 3, 0, 4, 8, 7, 6, 5)
+    val e = list.random()
+    val index = linearSearch(list = list, e = e)
+
+    println(e)
+    println(list)
+    println(index)
+}
+
+private fun <T : Comparable<T>> linearSearch(list: List<T>, e: T): Int {
+    for ((index, element) in list.withIndex()) {
+        if (element == e) return index
+    }
+
+    return -1
+}
