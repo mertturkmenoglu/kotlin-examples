5ab729048d5be0d9d393b661223d1982652cd376
Mert <mertturkmenoglu99@gmail.com>
2020-01-05 01:07:00 +0300

    Add Collections framework example

---
+++ kotlinExamples/src/examples/src/main/kotlin/collections/general/e008_JavaCollectionsFramework/Main.kt
@@ -0,0 +1,16 @@
+package collections.general.e008_JavaCollectionsFramework
+
+import java.util.*
+
+fun main() {
+    val list = arrayListOf(1, 1, 2, 3, 5)
+    list.shuffle() // Collections.shuffle()
+    println(list)
+
+    val count = Collections.frequency(list, 1)
+    println(count)
+
+    val list2 = arrayListOf(1, 2, 3, 4, 5)
+    val isDisjoint = Collections.disjoint(list, list2)
+    println(isDisjoint)
+}
