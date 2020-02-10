6b0eb7891438131bf4e1c82463acd33487b82bb8
Mert <mertturkmenoglu99@gmail.com>
2020-02-10 14:13:11 +0300

    Add string reverse practice example

---
+++ kotlinExamples/src/examples/src/main/kotlin/practice/problemSolving/e106_SpecialCharacterStringReverse/Main.kt
@@ -0,0 +1,36 @@
+package practice.problemSolving.e106_SpecialCharacterStringReverse
+
+import java.lang.StringBuilder
+
+fun main() {
+    val input = "emily!is$\$the3be4st"
+    val result = reverseAlphabetical(input)
+    // Expected output: tsebe!ht$$siy3li4me
+    println(result)
+}
+
+@Suppress("SameParameterValue")
+private fun reverseAlphabetical(str: String): String {
+    val sb = StringBuilder()
+    sb.append(str)
+    var l = 0
+    var r = sb.length - 1
+
+    while (l <= r) {
+        if (!sb[l].isLetter()) {
+            l++
+        }
+
+        if (!sb[r].isLetter()) {
+            r--
+        }
+
+        if (sb[l].isLetter() && sb[r].isLetter()) {
+            sb[l] = sb[r].also { sb[r] = sb[l] }
+            l++
+            r--
+        }
+    }
+
+    return sb.toString()
+}
---
+++ kotlinExamples/src/examples/src/main/kotlin/practice/problemSolving/e106_SpecialCharacterStringReverse/README.md
@@ -0,0 +1,2 @@
+* You are given a string that contains alphabetical characters and some other characters.
+* Reverse only the alphabetical characters.
